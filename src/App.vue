75394fb76eff63ab0a068a4190774f3d537facfe
serrgeyOkhr <ohryamkin.ser@yandex.ru>
2019-10-04 21:55:48 +0700

    Make Layout + Login + Categoris

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,30 @@
 <template>
   <div id="app">
-    <router-view/>
+    <component :is="layout">
+      <router-view/>
+    </component>
   </div>
 </template>
+
+<script>
+import EmptyLayout from "@/layouts/EmptyLayout";
+import MainLayout from "@/layouts/MainLayout";
+
+export default {
+  computed: {
+    layout() {
+      console.log(this.$route.meta);
+      return "empty-layout";
+    }
+  },
+  components: {
+    EmptyLayout,
+    MainLayout
+  }
+};
+</script> 
 
 <style lang="scss">
 @import "~materialize-css/dist/css/materialize.min.css";
 @import "assets/index.css";
-
-
 </style>
